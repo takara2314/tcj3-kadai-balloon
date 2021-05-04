8428c6827ce97212ea84aaf28ca05d13d6dc36bb
takara2314 <takara2314_TK@outlook.jp>
2021-05-04 21:29:41 +0900

    👨🏻‍🏫 Add talk response base

--- init.go
+++ init.go
@@ -8,14 +8,49 @@ import (
 )
 
 var (
-	bot         *linebot.Client
-	flexAddInfo []byte
+	bot               *linebot.Client
+	subjectList       []string
+	flexAddInfo       []byte
+	flexChangeSubject []byte
 )
 
 func init() {
 	var err error
 
+	subjectList = []string{
+		"国語３",
+		"現代社会",
+		"日本語教育１",
+		"日本語教育２",
+		"微分積分２",
+		"代数・幾何２",
+		"化学",
+		"保健体育３",
+		"Level Up English１",
+		"Level Up English２",
+		"プログラミング２",
+		"情報工学３",
+		"WEBアプリケーション",
+		"電気電子回路",
+		"工業力学１",
+		"材料学",
+		"機械製図",
+		"機械加工実習",
+		"マイコン工学",
+		"計測工学",
+		"工学数理基礎１",
+		"工学数理基礎２",
+		"キャリアデザイン１",
+		"PBL３",
+	}
+
 	flexAddInfo, err = ioutil.ReadFile("./templates/addInfo.json")
+	if err != nil {
+		log.Fatalln(err)
+		panic(err)
+	}
+
+	flexChangeSubject, err = ioutil.ReadFile("./templates/changeSubject.json")
 	if err != nil {
 		log.Fatalln(err)
 		panic(err)
--- replyTextMessage.go
+++ replyTextMessage.go
@@ -9,9 +9,9 @@ import (
 
 func replyTextMessage(event *linebot.Event, message string) {
 	var err error
-	var replyMessage string
 
 	if strings.HasPrefix(message, "add") {
+		var replyMessage string
 		var res *linebot.UserProfileResponse
 		var flexMessage linebot.FlexContainer
 
@@ -31,7 +31,7 @@ func replyTextMessage(event *linebot.Event, message string) {
 
 		_, err = bot.ReplyMessage(
 			event.ReplyToken,
-			linebot.NewFlexMessage("追加する課題情報", flexMessage),
+			linebot.NewFlexMessage("課題のタイトルを教えてください。", flexMessage),
 			linebot.NewTextMessage(replyMessage),
 		).Do()
 		if err != nil {
@@ -40,7 +40,7 @@ func replyTextMessage(event *linebot.Event, message string) {
 		}
 
 	} else if strings.HasPrefix(message, "change") {
-		replyMessage = "change OK"
+		var replyMessage string = "change OK"
 
 		_, err = bot.ReplyMessage(
 			event.ReplyToken,
@@ -52,7 +52,7 @@ func replyTextMessage(event *linebot.Event, message string) {
 		}
 
 	} else if strings.HasPrefix(message, "remove") {
-		replyMessage = "remove OK"
+		var replyMessage string = "remove OK"
 
 		_, err = bot.ReplyMessage(
 			event.ReplyToken,
@@ -64,7 +64,7 @@ func replyTextMessage(event *linebot.Event, message string) {
 		}
 
 	} else if strings.HasPrefix(message, "info") {
-		replyMessage = "info OK"
+		var replyMessage string = "info OK"
 
 		_, err = bot.ReplyMessage(
 			event.ReplyToken,
@@ -75,8 +75,137 @@ func replyTextMessage(event *linebot.Event, message string) {
 			panic(err)
 		}
 
+	} else if message == "[提出期限を変更]" {
+		var replyMessages []string = []string{
+			"課題の提出期限を設定してください。",
+
+			"" +
+				"「XX月XX日 XX時XX分」と打てばその日時に、「XX月XX日 XX限目」と打てばその時間が始まる日時に期限を設定できます。\n" +
+				"「XX月XX日」の代わりに「来週」や「今週○曜日」にすることもできます。",
+		}
+
+		_, err = bot.ReplyMessage(
+			event.ReplyToken,
+			linebot.NewTextMessage(replyMessages[0]),
+			linebot.NewTextMessage(replyMessages[1]),
+		).Do()
+		if err != nil {
+			log.Println(err)
+			panic(err)
+		}
+
+	} else if message == "[教科を変更]" {
+		var flexMessage linebot.FlexContainer
+
+		flexMessage, err = linebot.UnmarshalFlexMessageJSON(flexChangeSubject)
+		if err != nil {
+			log.Println(err)
+			panic(err)
+		}
+
+		_, err = bot.ReplyMessage(
+			event.ReplyToken,
+			linebot.NewFlexMessage("教科を選んでください。", flexMessage),
+		).Do()
+		if err != nil {
+			log.Println(err)
+			panic(err)
+		}
+
+	} else if isSubject(message) {
+		var replyMessages []string = []string{
+			"教科を設定しました。",
+
+			"課題の提出期限を設定してください。",
+
+			"" +
+				"「XX月XX日 XX時XX分」と打てばその日時に、「XX月XX日 XX限目」と打てばその時間が始まる日時に期限を設定できます。\n" +
+				"「XX月XX日」の代わりに「来週」や「今週○曜日」にすることもできます。",
+		}
+
+		_, err = bot.ReplyMessage(
+			event.ReplyToken,
+			linebot.NewTextMessage(replyMessages[0]),
+			linebot.NewTextMessage(replyMessages[1]),
+			linebot.NewTextMessage(replyMessages[2]),
+		).Do()
+		if err != nil {
+			log.Println(err)
+			panic(err)
+		}
+
+	} else if strings.HasPrefix(message, "others") {
+		var splited []string = strings.Split(message, ",")
+
+		if len(splited) == 2 {
+			var flexMessage linebot.FlexContainer
+
+			flexMessage, err = linebot.UnmarshalFlexMessageJSON(flexChangeSubject)
+			if err != nil {
+				log.Println(err)
+				panic(err)
+			}
+
+			_, err = bot.ReplyMessage(
+				event.ReplyToken,
+				linebot.NewFlexMessage("教科を選んでください。", flexMessage),
+			).Do()
+			if err != nil {
+				log.Println(err)
+				panic(err)
+			}
+
+		} else {
+			var replyMessage string = "「others,組」という形で送信してください。"
+
+			_, err = bot.ReplyMessage(
+				event.ReplyToken,
+				linebot.NewTextMessage(replyMessage),
+			).Do()
+			if err != nil {
+				log.Println(err)
+				panic(err)
+			}
+		}
+
+	} else if message == "A" {
+		var flexMessage linebot.FlexContainer
+
+		flexMessage, err = linebot.UnmarshalFlexMessageJSON(flexChangeSubject)
+		if err != nil {
+			log.Println(err)
+			panic(err)
+		}
+
+		_, err = bot.ReplyMessage(
+			event.ReplyToken,
+			linebot.NewFlexMessage("教科を選んでください。", flexMessage),
+		).Do()
+		if err != nil {
+			log.Println(err)
+			panic(err)
+		}
+
+	} else if message == "B" {
+		var flexMessage linebot.FlexContainer
+
+		flexMessage, err = linebot.UnmarshalFlexMessageJSON(flexChangeSubject)
+		if err != nil {
+			log.Println(err)
+			panic(err)
+		}
+
+		_, err = bot.ReplyMessage(
+			event.ReplyToken,
+			linebot.NewFlexMessage("教科を選んでください。", flexMessage),
+		).Do()
+		if err != nil {
+			log.Println(err)
+			panic(err)
+		}
+
 	} else {
-		replyMessage = "？"
+		var replyMessage string = "？"
 
 		_, err = bot.ReplyMessage(
 			event.ReplyToken,
@@ -87,4 +216,14 @@ func replyTextMessage(event *linebot.Event, message string) {
 			panic(err)
 		}
 	}
+}
+
+func isSubject(subject string) bool {
+	for _, str := range subjectList {
+		if str == subject {
+			return true
+		}
+	}
+
+	return false
 }
